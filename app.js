83f8eff26c1bedcb17a0c701d05fc370cf593547
Soumyadip Roy <email@gmail.com>
2024-01-21 17:13:52 +0530

    color picker initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const genCol = document.getElementById("gen");
+
+genCol.addEventListener("click", () => {
+  let h3 = document.querySelector("h3");
+  h3.innerText = getRandomColor();
+  let div = document.querySelector("div");
+  div.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+
+  let color = `rgb(${red},${green},${blue})`;
+  return color;
+}
